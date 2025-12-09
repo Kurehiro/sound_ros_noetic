277c6bfdb0bea21eaf75da9665781d81194f4a00
Kurehiro <kurehiro1009@gmail.com>
2025-12-09 13:28:36 +0900

    launchファイルの作成とそれに伴うwhisper部分の変更

--- sound_noetic/src/sound_send/scripts/whisper_node.py
+++ sound_noetic/src/sound_send/scripts/whisper_node.py
@@ -6,6 +6,7 @@ import os
 import sys
 import numpy as np
 import noisereduce as nr
+import subprocess
 
 from std_msgs.msg import String
 
@@ -14,6 +15,7 @@ class WhisperNode:
         rospy.init_node('whisper_node', anonymous=True)
         
         self.model_size = rospy.get_param('~model','medium')
+        self.device_index = rospy.get_param('~device_index', '')
         
         #マイクID設定
         self.mic_id = rospy.get_param('~mic_id', 'default_mic')
@@ -43,11 +45,23 @@ class WhisperNode:
         try:
             self.model = whisper.load_model(self.model_size, device=self.device)
             rospy.loginfo("model load: succeeded")
+            self.launch_sound_subsc()
         except Exception as e:
             rospy.logerr(f"model load: failed")
             sys.exit(1)
         
         rospy.spin()
+    
+    def launch_sound_subsc(self):
+        rospy.loginfo("Starting sound_subc.py after model load")
+        cmd = ["rosrun","sound_send","sound_subsc.py",f"_mic_id:={self.mic_id}"]
+        if self.device_index != "":
+            cmd.append(f"_device_index:={self.device_index}")
+        try:
+            subprocess.Popen(cmd)
+            rospy.loginfo("sound_subsc.py launched successfuly")
+        except Exception as e:
+            rospy.loginfo(f"Failed to launch sound_subsc.py: {e}")
     
     def process_audio(self, audio_data: np.ndarray, sr: int = 16000, normalize: bool = True, reduce_noise: bool = True) -> np.ndarray:
         processed_audio = audio_data.copy()
