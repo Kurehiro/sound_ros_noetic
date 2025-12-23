ff36283fad73eefac4c7ca635970bdebb3b09272
Kurehiro <kurehiro1009@gmail.com>
2025-12-23 10:56:06 +0900

    ROS_IPを自動で自身のIPアドレスに変換

--- sound_noetic/enter_docker.sh
+++ sound_noetic/enter_docker.sh
@@ -3,6 +3,11 @@
 # Xサーバーへのアクセスを許可
 xhost +local:docker
 
+# Auto-detect IP (first non-loopback interface)
+export ROS_IP=$(hostname -I | awk '{print $1}')
+
+echo "Detected IP: $ROS_IP"
+
 # コンテナをバックグラウンドで起動（存在しない場合は作成、停止している場合は起動）
 docker compose up -d
 
