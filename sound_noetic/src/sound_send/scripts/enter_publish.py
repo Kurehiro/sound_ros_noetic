b2a09e91bada3ea68a04724af16b4acdcf6afdc4
Kurehiro <kurehiro1009@gmail.com>
2026-04-19 21:30:58 +0900

    ちょい修正

--- sound_noetic/src/sound_send/scripts/enter_publish.py
+++ sound_noetic/src/sound_send/scripts/enter_publish.py
@@ -4,24 +4,26 @@ import rospy
 from std_msgs.msg import String
 
 
-def main():
-    rospy.init_node('enter_topic_publisher')
+class EnterPublisher:
+    @staticmethod
+    def main():
+        rospy.init_node('enter_topic_publisher')
 
-    pub = rospy.Publisher('/enter_trigger', String, queue_size=10)
+        pub = rospy.Publisher('/enter_trigger', String, queue_size=10)
 
-    rospy.loginfo('Enterキーを押すと /enter_trigger に "start" を publish します。')
-    rospy.loginfo('終了するときは Ctrl + C を押してください。')
+        rospy.loginfo('Enterキーを押すと /enter_trigger に "start" を publish します。')
+        rospy.loginfo('終了するときは Ctrl + C を押してください。')
 
-    while not rospy.is_shutdown():
-        try:
-            input()
-            pub.publish("start")
-            rospy.loginfo('Published: start')
-        except EOFError:
-            break
-        except KeyboardInterrupt:
-            break
+        while not rospy.is_shutdown():
+            try:
+                input()
+                pub.publish(String(data="start"))
+                rospy.loginfo('Published: start')
+            except EOFError:
+                break
+            except KeyboardInterrupt:
+                break
 
 
 if __name__ == '__main__':
-    main()
+    EnterPublisher.main()
